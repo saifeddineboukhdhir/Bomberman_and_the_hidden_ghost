69be0865dbfa7840341ee1da7947a5a5840cda38
[ Saif Eddine BOUKHDHIR] <[saifeddineboukhdhir@gmail.com]>
2019-03-25 09:10:11 +0100

    it works

--- maze.py
+++ maze.py
@@ -1,9 +1,30 @@
 # -*- coding: utf-8 -*-
 import arcade
-from models import World, Bomberman 
+
+from models import World, Bomberman
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+class MazeDrawer():
+    def __init__(self, maze):
+        self.maze = maze
+        self.width = self.maze.width
+        self.height = self.maze.height
+ 
+        self.wall_sprite = arcade.Sprite('images/wall.jpg')
+#        self.dot_sprite = arcade.Sprite('images/dot.png')
+ 
+    def draw(self):
+        for r in range(self.height):
+            for c in range(self.width):
+                x = c * 40 + 20;
+                y = r * 40 + 60;
+ 
+                if self.maze.has_wall_at(r,c):
+                    self.wall_sprite.set_position(x,y)
+                    self.wall_sprite.draw()
+#                elif self.maze.has_dot_at(r,c):
+#                    self.dot_sprite.set_position(x,y)
+#                    self.dot_sprite.draw()
 
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
@@ -23,15 +44,19 @@ class MazeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.AERO_BLUE)
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.bomberman_sprite = ModelSprite('images/bomberman.png',
                                          model=self.world.bomberman)
+        self.maze_drawer = MazeDrawer(self.world.maze)
     def update(self, delta):
         self.world.update(delta) 
     def on_draw(self):
         arcade.start_render()
+        self.maze_drawer.draw()
         self.bomberman_sprite.draw()
+    def on_key_press(self, key, key_modifiers):
+         self.world.on_key_press(key, key_modifiers)    
  
  
 def main():
--- models.py
+++ models.py
@@ -1,18 +1,67 @@
 # -*- coding: utf-8 -*-
+import arcade.key
+DIR_STILL = 0
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+MOVEMENT_SPEED = 4
+DIR_OFFSETS = { DIR_STILL: (0,0),
+                DIR_UP: (0,1),
+                DIR_RIGHT: (1,0),
+                DIR_DOWN: (0,-1),
+                DIR_LEFT: (-1,0) }
 class Bomberman:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+        self.direction = DIR_STILL
+ 
+    def move(self, direction):
+        self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
+        self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
  
     def update(self, delta):
-        pass
+        self.move(self.direction)
  
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height 
+        self.maze = Maze(self)
         self.bomberman = Bomberman(self, width // 2, height // 2)
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.UP:
+            self.bomberman.direction = DIR_UP
+        if key == arcade.key.DOWN:
+            self.bomberman.direction = DIR_DOWN
+        if key == arcade.key.LEFT:
+            self.bomberman.direction = DIR_LEFT
+        if key == arcade.key.RIGHT:
+            self.bomberman.direction = DIR_RIGHT   
  
     def update(self, delta):
         self.bomberman.update(delta)
+class Maze:
+    def __init__(self, world):
+        self.map = [ '####################',
+                     '#..................#',
+                     '#.###.###..###.###.#',
+                     '#.#...#......#...#.#',
+                     '#.#.###.####.###.#.#',
+                     '#.#.#..........#.#.#',
+                     '#.....###..###.....#',
+                     '#.#.#..........#.#.#',
+                     '#.#.###.####.###.#.#',
+                     '#.#...#......#...#.#',
+                     '#.###.###..###.###.#',
+                     '#..................#',
+                     '####################' ]
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+    def has_wall_at(self, r, c):
+        return self.map[r][c] == '#'
+# 
+#    def has_dot_at(self, r, c):
+#        return self.map[r][c] == '.'    
